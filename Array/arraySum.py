1006c7d87a5c85b1533b07087388490bdaaaf2e3
USER <pranit.rajgure123@gmail.com>
2026-02-06 21:32:16 +0530

    Sum of Array Elements

---
+++ Array/arraySum.py
@@ -0,0 +1,14 @@
+class Timepass:
+    def arraysum(self, nums):
+        total = 0
+        for i in nums:
+            total = total + i
+        return total
+
+
+
+
+
+nums = [644,665,56,56565,54545]
+obj = Timepass()
+print(obj.arraysum(nums))
