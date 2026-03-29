5408ce5ed6856fd79104c21f7b78118f66b80855
USER <pranit.rajgure123@gmail.com>
2026-03-29 18:21:03 +0530

    Given the root of a binary tree, return the postorder traversal of its nodes.
    What is Postorder?
    -Trevaersal order:
    Left → Right → Root

---
+++ Tree/Postorder Traversal.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def postorderTraversal(self, root):
+        result = []
+
+
+        def postorder(node):
+            if node is None:
+                return
+
+            postorder(node.left)
+            postorder(node.right)
+            result.append(node.val)
+
+        postorder(root)
+        return result
+
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+
+obj = Solution()
+print(obj.postorderTraversal(root))
